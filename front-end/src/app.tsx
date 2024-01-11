41d5c8a7b953353e780f95992ff1e0d0617801c6
doinel1a <doinel1atanasiu@gmail.com>
2024-01-11 14:44:50 +0100

    refactor(front-end): remove unused components

--- front-end/src/app.tsx
+++ front-end/src/app.tsx
@@ -2,29 +2,18 @@ import React from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
 
-import Counter from './components/counter';
 import Footer from './components/footer';
-import GithubCorner from './components/github-corner';
 import Navbar from './components/navbar';
-import ThemeProvider from './components/ui/theme/provider';
-import EStorageKeys from './constants/keys';
 
 function App() {
   return (
     <BrowserRouter>
-      <ThemeProvider defaultTheme='system' storageKey={EStorageKeys.theme}>
-        <Navbar />
+      <Navbar />
 
-        <main className='flex h-full flex-col items-center justify-center'>
-          <GithubCorner
-            title='Get started on GitHub'
-            url='https://github.com/doinel1a/vite-react-ts-shadcn-ui'
-          />
-          <Counter />
-        </main>
-
-        <Footer />
-      </ThemeProvider>
+      <main className='flex h-full flex-col items-center justify-center'>
+        <h1 className='text-2xl'>Starknet | DeFi Builder</h1>
+      </main>
+      <Footer />
     </BrowserRouter>
   );
 }
