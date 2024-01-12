ed7c5f3106fc7df94e7a06f88fb27cdf23b3807c
doinel1a <doinel1atanasiu@gmail.com>
2024-01-12 15:59:59 +0100

    feat(front-end): add home and 404 routes w/ lazy loading

--- front-end/src/app.tsx
+++ front-end/src/app.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Layout from './components/layout';
+import { Skeleton } from './components/ui/skeleton';
+
+const HomePage = React.lazy(() => import('./pages/home'));
+const FourOuFourPage = React.lazy(() => import('./pages/four-ou-four'));
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
-        <h1 className='text-2xl'>Starknet | DeFi Builder</h1>
+        <Suspense fallback={<Skeleton className='h-full w-full' />}>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='*' element={<FourOuFourPage />} />
+          </Routes>
+        </Suspense>
       </Layout>
     </BrowserRouter>
   );
---
+++ front-end/src/pages/home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <>
+      <h1 className='text-2xl'>Starknet | DeFi Builder </h1>
+    </>
+  );
+}
