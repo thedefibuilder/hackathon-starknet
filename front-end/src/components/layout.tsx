a64fde1d23dd88e0c5cab5b8ddf59bd8989cbf02
doinel1a <doinel1atanasiu@gmail.com>
2024-01-12 20:20:16 +0100

    refactor(front-end): layout styles

--- front-end/src/components/layout.tsx
+++ front-end/src/components/layout.tsx
@@ -8,7 +8,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <>
       <Navbar />
 
-      <main className='flex h-full flex-col items-center justify-center'>{children}</main>
+      <main className='flex h-full flex-col items-center overflow-y-auto px-2.5 py-2.5 xl:px-0'>
+        {children}
+      </main>
 
       <Footer />
     </>
